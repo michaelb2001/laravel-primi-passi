d429c9f75192f400a5f40b6952d0ee43d3d5d33c
michaelb2001 <brunco.michael@gmail.com>
2022-03-02 13:31:01 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+
+    <style>
+        lottie-files{
+            position:absolute;
+            top:30%;
+            left:15%;
+        }
+    </style>
+</head>
+
+<body>
+    
+<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+<lottie-player src="https://assets10.lottiefiles.com/packages/lf20_zvcqo3ed.json"  background="transparent"  speed="1"  style="width: 300px; height: 300px;"  loop autoplay></lottie-player>
+
+<h1>Laravel : la chiave per il futuro</h1>
+</body>
+</html>
